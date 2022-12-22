dff07530509960b72c536308b222eb000409f837
najmulhc <kridi32@outlook.com>
2022-12-22 19:13:06 +0600

    now we can parse data from JSON file and display that data using html from our node server

---
+++ user-server/server.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const http = require('http'); 
+const url = require('url')
+ 
+
+const userDisplayer = (file, user ) => {
+    let result = file.replace(/%USERNAME%/, user.name)  ;
+    result = result.replace("%COMPANY%", user.company.name)
+    result = result.replace("%WEBSITE%", user.website)
+    result = result.replace("%EMAIL%", user.email)
+    result = result.replace("%PHONE%", user.phone)
+    return result;
+}
+
+const server = http.createServer((req, res) => {
+    // server code will go here
+
+    // finding user id from url query... 
+    const user = parseInt(url.parse(req.url, true).query.user);
+ 
+    // here I have parsed the json data we have in the json file and stored that into a variable. 
+    const users = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
+ 
+    fs.readFile("./index.html", 'utf-8' , (err, html) => {
+        if(err){
+            console.error(err);
+            return
+        }  
+           if(user < 10){
+            const hostedFrom =  userDisplayer(html, users[user])
+            res.end(hostedFrom);
+           }
+             
+            return;
+        
+    })
+});
+
+
+server.listen(1234, () => {
+    console.log("server is running in the port" +' 127.0.0.1/1234')
+})

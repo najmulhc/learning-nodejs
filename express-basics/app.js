f5e731002c7742a6b4130e749eb4f42cfd8daf92
najmulhc <kridi32@outlook.com>
2022-12-28 21:37:11 +0600

    now we can take request parameters and response according to that

--- express-basics/app.js
+++ express-basics/app.js
@@ -2,48 +2,73 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-// a function that can modify the incoming the request data 
+// a function that can modify the incoming the request data
 app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status:"success", 
-        results: tours.length, 
-        data: {
-            tours: tours,
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+
+  const tour = tours.find((element) => element.id === req.params.id * 1);
+  if(!tour){
+    return res.status(404).json({
+        status: "failed", 
+        message: "could not found the desired tour!"
+    })
+ }
+  res.status(200).json({
+    status: 'success',
+    results: 1,
+    data: {
+      tour: tour,
+    },
+  });
 });
 
 app.post('/api/v1/tours', (req, res) => {
-    const newTour = req.body;
-    tours.push({
-        id: tours.length, 
-        ...newTour
-    });
-    const finalNewTour = JSON.stringify(tours);
-    fs.writeFile(`${__dirname}/dev-data/data/tours.json`, finalNewTour, (error , result) => {
-        if(error) {
-            res.status(503).json({
-                status:"failed", 
-                data: {
-                    message: "internal server error (while writing on the file)"
-                }
-            })
-        }
-        res.status(200).json({
-            status: "success", 
-            data : {
-               message: "the requested tour has been added into the database"
-            }
-        })
-    })
-})
+  const newTour = req.body;
+  tours.push({
+    id: tours.length,
+    ...newTour,
+  });
+  const finalNewTour = JSON.stringify(tours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    finalNewTour,
+    (error, result) => {
+      if (error) {
+        res.status(503).json({
+          status: 'failed',
+          data: {
+            message: 'internal server error (while writing on the file)',
+          },
+        });
+      }
+      res.status(200).json({
+        status: 'success',
+        data: {
+          message: 'the requested tour has been added into the database',
+        },
+      });
+    }
+  );
+});
+
+// handling parameters of HTTP request
 
 const port = 3000;
 app.listen(port, () => {
-    console.log('the server is running on the port:', port);
+  console.log('the server is running on the port:', port);
 });
-
